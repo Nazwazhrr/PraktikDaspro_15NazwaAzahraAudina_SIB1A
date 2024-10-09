fa67dd2311dcdb0ee278b51778b83dfaab939bb1
nazwazhrr <nazwaazahra883@gmail.com>
2024-10-09 19:20:42 +0700

    modifikasi percobaan 2

--- Daspro_jobsheet6/pemilihan2percobaan115.java
+++ Daspro_jobsheet6/pemilihan2percobaan115.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
 /**
- * Pemilihan2Percobaan115
+ * pemilihan2percobaan115
  */
+public class pemilihan2percobaan115 {
 
- /**
-  * pemilihan2percobaan115
-  */
- public class pemilihan2percobaan115 {
-     public static void main(String[] args) {
-         Scanner input15 = new Scanner(System.in);
-    
-         System.out.print("Masukkan tahun : ");
-         int tahun = input15.nextInt();
-    
-     if ((tahun % 4== 0 && tahun % 100 !=0) || (tahun % 400 == 0)) {
-        System.out.println("Tahun Kabisat");
-     }else
-     System.out.println("Bukan Tahun Kabisat");
-     }    
+    public static void main(String[] args) {
+        Scanner input15 = new Scanner(System.in);
+
+        System.out.print("Masukkan tahun : ");
+        int tahun = input15.nextInt();
+
+        // Cek apakah tahun habis dibagi 400 terlebih dahulu
+        if (tahun % 400 == 0) {
+            System.out.println("Tahun Kabisat");
+        } 
+        // Jika tidak, cek apakah habis dibagi 100
+        else {
+            if (tahun % 100 == 0) {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+            // Jika tidak habis dibagi 100, cek apakah habis dibagi 4
+            else {
+                if (tahun % 4 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            }
+        }
+    }
 }
